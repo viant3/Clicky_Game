76c317ae4aace8d3d01f3e2aef69b23ce4dcfc1c
victor politano <viant3ucf@gmail.com>
2019-10-30 16:47:47 -0400

    image change

--- src/images.js
+++ src/images.js
@@ -1,29 +1,29 @@
-import beth from './images/beth.png';
-import birdperson from './images/birdperson.png';
-import evilmorty from './images/evilmorty.png';
-import gianthead from './images/gianthead.png';
-import goldenford from './images/goldenford.png';
-import jerry from './images/jerry.png';
-import jessica from './images/jessica.png';
-import meeseeks from './images/meeseeks.png';
-import morty from './images/morty.png';
-import mr from './images/mr.png';
-import rick from './images/rick.png';
-import summer from './images/summer.png';
+import c1 from './images/c1.PNG';
+import c2 from './images/c2.PNG';
+import c3 from './images/c3.PNG';
+import c4 from './images/c4.PNG';
+import c5 from './images/c5.PNG';
+import c6 from './images/c6.PNG';
+import c7 from './images/c7.PNG';
+import c8 from './images/c8.PNG';
+import c9 from './images/c9.PNG';
+import c10 from './images/c10.PNG';
+import c11 from './images/c11.PNG';
+import c12 from './images/c12.PNG';
 
 const images = [
-  beth,
-  birdperson,
-  evilmorty,
-  gianthead,
-  goldenford,
-  jerry,
-  jessica,
-  meeseeks,
-  morty,
-  mr,
-  rick,
-  summer
+  c1,
+  c2,
+  c3,
+  c4,
+  c5,
+  c6,
+  c7,
+  c8,
+  c9,
+  c10,
+  c11,
+  c12
 ];
 
 export default images;
