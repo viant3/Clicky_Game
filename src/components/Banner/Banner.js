9a5f22bb3404885427ca882ba74c8f984a013f98
victor politano <viant3ucf@gmail.com>
2019-10-30 04:57:43 -0400

    created tree

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,8 @@ import './Banner.css';
 
 const Banner = () => (
   <div className="banner text-center d-flex align-items-center justify-content-center">
-    <p className="m-0">Click on an image to earn points, but don't click on any more than once!</p>
+    <p className="m-0 toBottom">Click on an image to earn points, but don't click on any more than once!</p>
   </div>
 );
+
 export default Banner;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import beth from './images/beth.png';
+import birdperson from './images/birdperson.png';
+import evilmorty from './images/evilmorty.png';
+import gianthead from './images/gianthead.png';
+import goldenford from './images/goldenford.png';
+import jerry from './images/jerry.png';
+import jessica from './images/jessica.png';
+import meeseeks from './images/meeseeks.png';
+import morty from './images/morty.png';
+import mr from './images/mr.png';
+import rick from './images/rick.png';
+import summer from './images/summer.png';
+
+const images = [
+  beth,
+  birdperson,
+  evilmorty,
+  gianthead,
+  goldenford,
+  jerry,
+  jessica,
+  meeseeks,
+  morty,
+  mr,
+  rick,
+  summer
+];
+
+export default images;
